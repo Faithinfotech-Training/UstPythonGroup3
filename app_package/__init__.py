49da1155ad63c0966467e98087a9b69e21d629e6
Meghag123 <meghag2697@gmail.com>
2020-03-09 11:31:46 +0530

    commit by megha

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -11,4 +11,4 @@ db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 mongo=PyMongo(app)
 
-from app_package import resource_routes,batch_routes,course_routes,routes,enquiry_routes
+from app_package import resource_routes,batch_routes,course_routes,routes,enquiry_routes,qualification_routes
